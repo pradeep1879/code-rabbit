070930820f12ff926c027fb46f8f9433289bd3c7
Pradeep Kumar <pk6949137@gmail.com>
2026-05-17 13:16:17 +0530

    add prisma and athentication ui

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginUI from '@/module/components/login-ui'
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+      <LoginUI/>
+    </div>
+  )
+}
+
+export default LoginPage
